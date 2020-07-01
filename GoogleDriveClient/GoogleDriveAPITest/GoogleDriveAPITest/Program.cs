91d76275ea65c7e40cc2cf3e9d68be1fb5fccada
Simon Geier <super.zockerhd@web.de>
2020-07-01 06:31:31 +0200

    Added GoogleDriveAPI download and upload functions (python)

---
+++ GoogleDriveClient/GoogleDriveAPITest/GoogleDriveAPITest/Program.cs
@@ -0,0 +1,192 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace DriveQuickstart
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/drive-dotnet-quickstart.json
+        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string textBox2 = "";
+
+        #region Coppied-Auth-Download
+        /// <summary>
+        /// This method requests Authentcation from a user using Oauth2.  
+        /// Credentials are stored in System.Environment.SpecialFolder.Personal
+        /// Documentation https://developers.google.com/accounts/docs/OAuth2
+        /// </summary>
+        /// <param name="clientSecretJson">Path to the client secret json file from Google Developers console.</param>
+        /// <param name="userName">Identifying string for the user who is being authentcated.</param>
+        /// <returns>DriveService used to make requests against the Drive API</returns>
+        public static DriveService AuthenticateOauth(string clientSecretJson, string userName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(userName))
+                    throw new ArgumentNullException("userName");
+                if (string.IsNullOrEmpty(clientSecretJson))
+                    throw new ArgumentNullException("clientSecretJson");
+                if (!System.IO.File.Exists(clientSecretJson))
+                    throw new Exception("clientSecretJson file does not exist.");
+
+                // These are the scopes of permissions you need. It is best to request only what you need and not all of them
+                string[] scopes = new string[] { DriveService.Scope.DriveReadonly };         	//View the files in your Google Drive                                                 
+                UserCredential credential;
+                using (var stream = new System.IO.FileStream(clientSecretJson, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                    credPath = System.IO.Path.Combine(credPath, ".credentials/", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+
+                    // Requesting Authentication or loading previously stored authentication for userName
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets,
+                                                                             scopes,
+                                                                             userName,
+                                                                             CancellationToken.None,
+                                                                             new FileDataStore(credPath, true)).Result;
+                }
+
+                // Create Drive API service.
+                return new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Drive Oauth2 Authentication Sample"
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Create Oauth2 account DriveService failed" + ex.Message);
+                throw new Exception("CreateServiceAccountDriveFailed", ex);
+            }
+        }
+
+        private static void SaveStream(System.IO.MemoryStream stream, string saveTo)
+        {
+            using (System.IO.FileStream file = new System.IO.FileStream(saveTo, System.IO.FileMode.Create, System.IO.FileAccess.Write))
+            {
+                stream.WriteTo(file);
+            }
+        }
+    
+        private static void DownloadFile(Google.Apis.Drive.v3.DriveService service, Google.Apis.Drive.v3.Data.File file, string saveTo)
+        {
+
+            var request = service.Files.Get(file.Id);
+            var stream = new System.IO.MemoryStream();
+
+            // Add a handler which will be notified on progress changes.
+            // It will notify on each chunk download and when the
+            // download is completed or failed.
+            request.MediaDownloader.ProgressChanged += (Google.Apis.Download.IDownloadProgress progress) =>
+            {
+                switch (progress.Status)
+                {
+                    case Google.Apis.Download.DownloadStatus.Downloading:
+                        {
+                            Console.WriteLine(progress.BytesDownloaded);
+                            break;
+                        }
+                    case Google.Apis.Download.DownloadStatus.Completed:
+                        {
+                            Console.WriteLine("Download complete.");
+                            SaveStream(stream, saveTo);
+                            break;
+                        }
+                    case Google.Apis.Download.DownloadStatus.Failed:
+                        {
+                            Console.WriteLine("Download failed.");
+                            break;
+                        }
+                }
+            };
+            request.Download(stream);
+
+        }
+        #endregion
+
+        #region Coppied-Upload
+        #endregion
+
+        public static void DownloadExpTrcFile(String path)
+        {
+            var service = AuthenticateOauth(@"credentials.json", "test");
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+
+            foreach (var item in files)
+            {
+                if (item.Name == "Data.json")
+                {
+                    DownloadFile(service, item, path);
+                    Console.WriteLine(item.Id);
+                }
+            }
+        }
+
+        private static File updateFile(DriveService service, String fileId, String newTitle,
+                                        String newDescription, String newMimeType, String newFilename, bool newRevision)
+        {
+            try
+            {
+                // First retrieve the file from the API.
+                File file = service.Files.Get(fileId).Execute();
+
+                // File's new metadata.
+                file.Name = newTitle;
+                file.Description = newDescription;
+                file.MimeType = newMimeType;
+
+                // File's new content.
+                byte[] byteArray = System.IO.File.ReadAllBytes(newFilename);
+                System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+                // Send the request to the API.
+                FilesResource.UpdateMediaUpload request = service.Files.Update(file, fileId, stream, newMimeType);
+                //request.NewRevision = newRevision;
+                request.Upload();
+
+                File updatedFile = request.ResponseBody;
+                return updatedFile;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                return null;
+            }
+        }
+
+
+        public static void UploadExpTrcFile(String path)
+        {
+            var service = AuthenticateOauth(@"credentials.json", "test");
+            var response = updateFile(service, "1ZbmIFV04JqNt1l5A_4qozCU4x-oHYox1", "Data2.json", "", "application/json", path, false);
+            var response2 = updateFile(service, "12SuDFHsemT4Ig_iwnqyIsf0GE7F1TmCz", "Data2.json", "", "application/json", path, false);
+            Console.WriteLine(response);
+            Console.WriteLine(response2);
+        }
+
+
+        static void Main(string[] args)
+        {
+            //DownloadExpTrcFile("C:/dev/ProgramFiles/ExpTrc/Data.json");
+            UploadExpTrcFile("C:/dev/ProgramFiles/ExpTrc/Data.json");
+        }
+
+    }
+}
